5fb3d0c078ab55049140d7a419c168cc36b78552
Hamza DRIOUCH <hdri@cairful.com>
2022-06-01 14:26:59 +0100

    Add Home view model

---
+++ CfUnitTestProjectDemo/ViewModels/BindableBase.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CfUnitTestProjectDemo.ViewModels
+{
+    public abstract class BindableBase : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        #region Fields
+
+        private readonly HashSet<string> _validationInitialized = new HashSet<string>();
+
+        private readonly ConcurrentDictionary<string, ICollection<object>> _validationErrors =
+            new ConcurrentDictionary<string, ICollection<object>>();
+
+        #endregion
+
+        #region Properties
+
+        [Browsable(false)]
+        public bool HasErrors => _validationErrors.Count > 0;
+
+        [Browsable(false)]
+        public bool HasChanged { get; set; } = false;
+
+
+        [Browsable(false)]
+        public IReadOnlyDictionary<string, IReadOnlyCollection<object>> ValidationErrors =>
+            new ReadOnlyDictionary<string, IReadOnlyCollection<object>>(
+                this._validationErrors
+                    .ToDictionary(
+                        x => x.Key,
+                        x => (IReadOnlyCollection<object>)new ReadOnlyCollection<object>(x.Value.ToList())));
+
+        #endregion
+
+        #region Events and Delegates
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Interface Implementations
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName) ||
+                this._validationErrors.ContainsKey(propertyName) == false)
+            {
+                return null;
+            }
+
+            return _validationErrors[propertyName];
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected virtual void OnErrorsChanged()
+        {
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+ 
+        protected void RaiseErrorsChanged([CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                foreach (var error in this._validationErrors.Keys.ToArray())
+                {
+                    ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(error));
+                }
+            }
+            else
+            {
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+            }
+
+            this.OnErrorsChanged();
+        }
+
+        [Obsolete("use OnPropertyChanged instead")]
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.OnPropertyChanged(propertyName);
+        }
+
+        protected void SetPropertyValue<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            property = value;
+            OnPropertyChanged(propertyName);
+        }
+
+
+
+
+        #endregion
+    }
+}
--- CfUnitTestProjectDemo/ViewModels/HomeViewVm.cs
+++ CfUnitTestProjectDemo/ViewModels/HomeViewVm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CfUnitTestProjectDemo.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,74 @@ using System.Threading.Tasks;
 
 namespace CfUnitTestProjectDemo.ViewModels
 {
-    public class HomeViewVm
+    public class HomeViewVm : BindableBase
     {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        public HomeViewVm()
+        {
+            SeeAllMemberCommand = new DelegateCommand(SeeAllMember);
+            AddMemberCommand = new DelegateCommand(AddMember);
+            CancelMemberCommand = new DelegateCommand(CancelMember);
+            GenerateCodeCommand = new DelegateCommand(GenerateCode);
+        }
+
+        public DelegateCommand AddMemberCommand { get; set; }
+        public DelegateCommand CancelMemberCommand { get; set; }
+        public DelegateCommand GenerateCodeCommand { get; set; }
+        public DelegateCommand SeeAllMemberCommand { get; set; }
+
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    OnPropertyChanged(nameof(FirstName));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                SetPropertyValue(ref _lastName, value);
+            }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                SetPropertyValue(ref _email, value);
+            }
+        }
+
+        public void AddMember()
+        {
+
+        }
+        private void GenerateCode()
+        {
+            
+        }
+
+        private void CancelMember()
+        {
+            
+        }
+
+        private void SeeAllMember()
+        {
+            
+        }
     }
 }
