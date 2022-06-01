eafb4482dd62732bba79a9026220fd91bef206ac
Abdelhanine Mouraji <amou@cairful.com>
2022-06-01 15:39:35 +0100

    Add Services

---
+++ CfUnitTestProjectDemo/Services/WriteAsync.cs
@@ -0,0 +1,29 @@
+﻿using CfUnitTestProjectDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CfUnitTestProjectDemo.Services
+{
+    public class WriteAsync
+    {
+        /// <summary>
+        /// Method that writes a new member to a textfile.
+        /// </summary>
+        /// <param name="member">Member to write to file</param>
+        /// <param name="filepath">Filepath of the textfile</param>
+        /// <returns>Bool(Return of this method is only there for testing)</returns>
+        public async Task<bool> WriteToFileAsync(Member member, string filepath)
+        {
+            using (StreamWriter fileWriter = new StreamWriter(filepath, append: true))
+            {
+                await fileWriter.WriteLineAsync($"{member.Id} {member.FullName} {member.Email}");
+                fileWriter.Close();
+                return true;
+            }
+        }
+    }
+}
