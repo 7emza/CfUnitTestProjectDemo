f7667399b3684e54bb9f36cd17a141d11eef4597
Hamza DRIOUCH <hdri@cairful.com>
2022-06-01 14:48:41 +0100

    Add Member Model

---
+++ CfUnitTestProjectDemo/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CfUnitTestProjectDemo.Models
+{
+    public class Member
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
